29828874cfdfeae9c39fd0c965c5cafc2b8fb558
OscarSantos1 <ossantos1994@gmail.com>
2023-03-14 04:08:41 -0600

    confirmation with google app & nodemailer

--- backend/controllers/emailControllers.js
+++ backend/controllers/emailControllers.js
@@ -1,29 +1,39 @@
 const nodemailer = require("nodemailer");
 const User = require("../models/userModel");
-
-const transporter = nodemailer.createTransport({
-  host: "smtp-mail.outlook.com", // hostname
-  service: "outlook", // service name
-  secureConnection: false,
-  tls: {
-    ciphers: "SSLv3", // tls version
-  },
-  port: 587, // port
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
-  },
-});
+const { google } = require("googleapis");
 
 const sendConfirmation = async (req, res) => {
   try {
+    const html = req.body.html;
     const { email } = await User.findById(req.user.id);
+    const oAuth2Client = new google.auth.OAuth2(
+      process.env.CLIENT_ID,
+      process.env.CLIENT_SECRET,
+      process.env.REDIRECT_URI
+    );
+
+    oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
+
+    const accessToken = await oAuth2Client.getAccessToken();
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.EMAIL,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: accessToken,
+      },
+    });
     const options = {
       from: process.env.EMAIL_SENDER,
       to: email,
       subject: "Thank you for your purchase!",
-      html: req.body.html,
+      html: html,
     };
+
     await transporter.sendMail(options);
     res.status(200).send();
   } catch (error) {
